39e105a22a93fbfecebadcff636c3d7fc5a0992d
rachel howard <rdfields13@gmail.com>
2023-01-31 15:35:34 -0600

    created portfolio with react and tailwinds

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to to-black text-white'>
+
+<div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-centerw-full h-full'>
+    <div className='pb-8' >
+        <p className='text-4xl font-bold inline border-b-4 border-gray-500 '>
+            About
+        </p>
+    </div>
+    <p className='text-xl mt-20'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate, fuga. Consectetur eos asperiores saepe, iusto facilis sit error officia, natus quia eum minima dolorum ullam, recusandae expedita ducimus sint fuga aut deleniti numquam atque eius suscipit ut pariatur! Nobis quisquam deleniti at asperiores, veniam magni officiis officia corporis ut facere?
+    </p>
+
+    <br />
+    <p className='text-xl'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi aperiam ea, omnis voluptas provident deserunt perferendis sed officiis harum enim accusantium ab dolorem, ullam quibusdam fugit eaque expedita vitae voluptates, officia laboriosam culpa esse eius est. Eum quasi molestias accusantium consectetur ad asperiores sapiente eius illo, deleniti, eos sed eligendi?3
+    </p>
+</div>
+
+    </div>
+  )
+}
+
+export default About
+
